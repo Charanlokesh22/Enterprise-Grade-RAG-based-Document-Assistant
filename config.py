a472c8bc5168365de7a7b10f6ffc3dc87d6498fb
Charanlokesh22 <charanlokesh2918@gmail.com>
2025-08-15 16:33:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config:
     ARIZE_SPACE_KEY = os.getenv("ARIZE_SPACE_KEY")
     
     
+    
 
     WHYLABS_API_KEY = os.getenv("WHYLABS_API_KEY")
     WHYLABS_ORG_ID = os.getenv("WHYLABS_ORG_ID")
